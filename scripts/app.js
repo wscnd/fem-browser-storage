3e05d205f571ff6aa270b99a677aa5cbf5888bf4
wscnd <72019998+wscnd@users.noreply.github.com>
2024-02-08 12:38:36 -0300

    lint

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
-import Menu from './Menu.js';
-import Order from './Order.js';
-import Router from './Router.js';
+import Menu from "./Menu.js";
+import Order from "./Order.js";
+import Router from "./Router.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-    Router.init();
-    Menu.load();
-    Order.render();
- } );
+  Router.init();
+  Menu.load();
+  Order.render();
+});
+
